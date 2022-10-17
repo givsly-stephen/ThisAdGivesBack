01bdbaa0645c779ee6c27a27a550e570abc1eb35
Stephen Anderson <stephen@givsly.com>
2022-10-17 23:50:22 +0300

    improved script to support multiple ads in one page

--- givsly.js
+++ givsly.js
@@ -1,32 +1,21 @@
 function injectable(data) {
     return `
     <style>
-    body { margin: 0; padding: 0; }
-    #givsly {
-        font-family: Arial, Helvetica, sans-serif;
-        font-size: 12px;
+    #givsly {z-index: 99999;top:0; left:0;position: absolute;}
+    #givsly #act {
+        z-index: 99999;display: flex;cursor:pointer;
+        position:absolute;
+        ${givslyConfig.activate.position};
     }
-    #givsly #overlay { 
-        width: ${givslyConfig.expose.width}; 
-        height: ${givslyConfig.expose.height};
-        z-index: 99999;
-        position: fixed;
-        ${givslyConfig.expose.position};
-        background-color: #eee;
-        display: flex;
-        align-items: center;
-        justify-content: center;  
-        cursor:pointer;
-
+    #givsly #act #tagb {
+        width: ${givslyConfig.activate.width}; 
+        height: ${givslyConfig.activate.height};
     }
-    #givsly #overlay #inner { 
-        text-align: center;
-    }
-    #givsly #overlay #l2 { font-weight: bold; font-size: 13px;  }
-    #givsly #interact {
-        font-size: 26px;
+    
+    #givsly #experience {
+        font-size: 20px;
         width: auto; 
-        height: ${givslyConfig.creative.height};
+        height: 450px;
         z-index: 99998;
         position: fixed;
         bottom: 0; right: 0; left: 0;
@@ -36,7 +25,7 @@ function injectable(data) {
         display: none;
         padding: 20px ;
     }
-    #givsly #interact li {
+    #givsly #experience li {
         border: 3px solid orange;
         border-radius: 40px;
         font-weight: bold;
@@ -46,58 +35,72 @@ function injectable(data) {
         list-style: none;
         cursor:pointer;
     }
-    #givsly #header {
+
+    #givsly  #experience #header {
         height: 40px;
     }
-    #givsly #npolist {
+
+    #givsly  #experience #npolist {
         padding: 0; margin: 0;
     }
-    #givsly #interact #close {
+
+    #givsly #experience #close {
         height: 100%;
         width: 40px;
         position: fixed;
         right: 20px;
         cursor:pointer;
     }
+
+
 </style>
 
-    <div id="givsly">
-    <div id="overlay" onclick="expose(this)"> 
-        <div id="inner">
-            <div id="l1">This ad</div>  
-            <div id="l2">GIVES BACK</div>
+    <div id="givsly" style="width: ${givslyConfig.ad.width}; height: ${givslyConfig.ad.height};">
+        <div id="act" onclick="activate(this)"> 
+            <img src="../tagb.png" id="tagb"/>
+        </div>
+        <div id="experience" > 
+            <div id="header">
+                <div id="close" onclick="closewindow(this)">X</div>
+            </div>
+            ${givslyConfig.message}
+            <ul id="npolist" />
         </div>
     </div>
-
-    <div id="interact" > 
-        <div id="header">
-            <div id="close" onclick="closewindow(this)">X</div>
-        </div>
-        
-        ${givslyConfig.message}
-
-        <ul id="npolist" />
-
-    </div>
-</div>
 `
 }
 
-if (typeof givslyConfig === 'undefined') {
-    alert("Givsly good ads not configured - missing `givslyConfig`")
-} else {
+var experience = null;
+function injectTAGB() {
+    if (typeof givslyConfig === 'undefined') {
+        alert("Givsly good ads not configured - missing `givslyConfig`")
+        return
+    }
+
     const payload = injectable(givslyConfig);
-    document.querySelector(':root').insertAdjacentHTML('beforeend', payload);
+
+    const ad_container = document.querySelector(':root')
+    if (!ad_container) {
+        alert("could not find div " + ad_container)
+        return
+    } 
+    ad_container.insertAdjacentHTML('beforeend', payload);
+    
     const npolist = document.getElementById('npolist')
     givslyConfig.npos.forEach(function(npo) {
         npolist.insertAdjacentHTML("beforeend", `<li onclick="selectNpo(this, '${npo.id}')">${npo.name}</li>`);
-    });
+    })
+   
+    experience = document.getElementById('experience');
 }
+
  
-const elem = document.getElementById('interact');
-function expose() {elem.style.display = "block";}
-function closewindow() {elem.style.display = "none";}
+function activate() { experience.style.display = "block";}
+function closewindow() {experience.style.display = "none";}
 function selectNpo(lineItem, npoId) {
     lineItem.style.background = "orange"
-    fetch(givslyConfig.npoev+npoId)
+    fetch(givslyConfig.npoev + npoId)
 }
+
+// script load using defer should call this after load
+injectTAGB()
